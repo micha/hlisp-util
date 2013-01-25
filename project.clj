c0290918f14967e0f359421aa83417dd88ecea20
Micha Niskin <asdf@ubergibson.com>
2013-01-25 17:45:02 -0500

    oops!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hlisp-flapjax "0.1.0-SNAPSHOT"
+(defproject hlisp-util "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://github.com/micha/hlisp-util"
   :license {:name "Eclipse Public License"
