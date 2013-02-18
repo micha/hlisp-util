28b1b753928b78322ec400e5e632b56e42dc0c8f
Micha Niskin <asdf@ubergibson.com>
2013-02-18 18:44:51 -0500

    fix project.clj to use cljsbuild

--- project.clj
+++ project.clj
@@ -1,8 +1,30 @@
 (defproject hlisp-util "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://github.com/micha/hlisp-util"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :manifest {"hlisp-provides" "util"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hlisp-macros "0.1.0-SNAPSHOT"]])
+  :description
+    "FIXME: write description"
+
+  :url 
+    "http://github.com/micha/hlisp-util"
+
+  :license 
+    {:name "Eclipse Public License"
+     :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies
+    []
+
+  :plugins
+    [[lein-cljsbuild "0.3.0"]]
+            
+  :source-paths
+    ["src/clj" "src/cljs"]
+
+  :cljsbuild
+    {:builds {:test {:source-paths ["src/clj" "src/cljs" "test"]
+                     :compiler {:output-to "test/test.js"
+                                :optimizations :advanced
+                                :warnings true
+                                ;; :optimizations :whitespace
+                                ;; :pretty-print true
+                                }
+                     :jar false}}
+     :test-commands {"unit" ["phantomjs" "test/runner.js"]}})
