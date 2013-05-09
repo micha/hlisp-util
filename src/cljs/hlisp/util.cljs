6b0e28664d032f1147aba064c614d7a3472667a6
Micha Niskin <asdf@ubergibson.com>
2013-05-09 13:29:01 -0400

    Add query fn, returns query string as map or lookup

--- src/cljs/hlisp/util.cljs
+++ src/cljs/hlisp/util.cljs
@@ -4,6 +4,27 @@
   (:require
     [clojure.string   :as string]))
 
+(defn safe-name
+  [x]
+  (try (name x) (catch js/Error e)))
+
+(let [qcache (atom ::none)]
+  (defn query
+    [& [k & _ :as args]]
+    (if-not (= ::none @qcache)
+      (if k (@qcache (safe-name k)) @qcache)
+      (let [s (-> js/window .-location .-search)]
+        (if (not (string/blank? s))
+          (let [v (-> s (string/replace #"^\?" "") (string/split #"[&]"))
+                m (->> v
+                    (mapv #(string/split % #"[=]"))
+                    (mapv #(mapv js/decodeURIComponent %))
+                    (remove #(or (= [""] %) (= 0 (count %)) (< 2 (count %))))
+                    (mapv #(if (< (count %) 2) (conj % "") %))
+                    (into {}))]
+            (reset! qcache m)
+            (apply query args)))))))
+
 (defn pluralize
   [word count]
   (str word (when (not= 1 count) "s")))
